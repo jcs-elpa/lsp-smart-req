cd0c4989ec3a8ae7487edbe45915f3afc60da067
JenChieh <jcs090218@gmail.com>
2025-02-09 22:07:21 -0800

    fix syntax

--- lsp-smart-req.el
+++ lsp-smart-req.el
@@ -153,7 +153,7 @@
     ((swift-mode swift-ts-mode)          . lsp-sourcekit)
     (terraform-mode                      . lsp-terraform)
     (tilt-mode                           . lsp-tilt)
-    (conf-toml-mode toml-ts-mode         . lsp-toml)
+    ((conf-toml-mode toml-ts-mode)       . lsp-toml)
     ((typespec-mode typespec-ts-mode)    . lsp-typespec)
     (scheme                              . lsp-ts-query)
     (ttcn3                               . lsp-ttcn3)
