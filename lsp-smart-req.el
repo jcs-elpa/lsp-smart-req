2316b59f186241ce6adf7d56d2817e44b8c7cc0c
JenChieh <jcs090218@gmail.com>
2026-05-03 20:30:25 +0800

    feat: Drop Emacs 28.x

--- lsp-smart-req.el
+++ lsp-smart-req.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/jcs-elpa/lsp-smart-req
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "27.1"))
+;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: internal lsp
 
 ;; This file is not part of GNU Emacs.
